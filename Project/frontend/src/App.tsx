d1506fb3aa01467d29f210f0b1dac69dc184d713
Sandeepkumar-Ramesh <pilotsandeep16@gmail.com>
2024-05-13 23:38:34 +0530

    Frontend App Setup

---
+++ Project/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import logo from './assets/team-logo.svg'
+
+function App() {
+
+
+  return (
+    <>
+      <h2>
+        Amos Project SS24 - Knowledge Graph Extractor
+      </h2>
+      <img className="logo" src={logo} alt="" />
+    </>
+  )
+}
+
+export default App
