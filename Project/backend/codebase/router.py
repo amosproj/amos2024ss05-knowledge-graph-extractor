5b3d2a91fc4e8fb92e71498bb12ec52293405bf4
Irem Ozseker <iremozseker@gmail.com>
2024-05-13 23:10:06 +0200

    saved the uploaded file locally (for now)
    Signed-off-by: Irem Ozseker <iremozseker@gmail.com>

--- Project/backend/codebase/router.py
+++ Project/backend/codebase/router.py
@@ -2,6 +2,7 @@ from fastapi.routing import APIRouter
 from fastapi import UploadFile, File, HTTPException
 from pathlib import Path
 
+import os
 import monitoring
 
 api_router = APIRouter()
@@ -14,5 +15,11 @@ async def upload_pdf(file: UploadFile = File(...)):
     # Check if the uploaded file type is correct
     if not Path(file.filename).suffix.lower() == ".pdf":
         raise HTTPException(status_code=400, detail="Uploaded file is not a PDF.")
+
+    # Save file locally
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_directory, file.filename)
+    with open(file_path, "wb") as f:
+        f.write(file.file.read())
 
     return {"filename": file.filename, "status": "uploaded successfully"}
