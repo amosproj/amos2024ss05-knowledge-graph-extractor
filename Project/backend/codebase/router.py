618c31e4d512016ef3c5526c4a859c9d6617f437
Irem Ozseker <iremozseker@gmail.com>
2024-05-13 22:36:30 +0200

    added upload endpoint to router.py
    Signed-off-by: Irem Ozseker <iremozseker@gmail.com>

--- Project/backend/codebase/router.py
+++ Project/backend/codebase/router.py
@@ -1,6 +1,18 @@
 from fastapi.routing import APIRouter
+from fastapi import UploadFile, File, HTTPException
+from pathlib import Path
 
 import monitoring
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router, prefix="/monitoring", tags=["monitoring"])
+
+
+# Endpoint for uploading PDF documents
+@api_router.post("/upload/")
+async def upload_pdf(file: UploadFile = File(...)):
+    # Check if the uploaded file type is correct
+    if not Path(file.filename).suffix.lower() == ".pdf":
+        raise HTTPException(status_code=400, detail="Uploaded file is not a PDF.")
+
+    return {"filename": file.filename, "status": "uploaded successfully"}
