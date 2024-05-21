410a6f9d9f055e109c974a12fc2b06fab612f3f3
Irem Ozseker <iremozseker@gmail.com>
2024-05-22 00:24:00 +0200

    fixed DeprecationWarnings
    Signed-off-by: Irem Ozseker <iremozseker@gmail.com>

--- Project/backend/codebase/lifetime.py
+++ Project/backend/codebase/lifetime.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from typing import Awaitable, Callable
 
 from fastapi import FastAPI
@@ -21,6 +22,7 @@ def _setup_db(app: FastAPI) -> None:  # pragma: no cover
         engine,
         expire_on_commit=False,
     )
+    print("_setup_db")
     app.state.db_engine = engine
     app.state.db_session_factory = session_factory
 
@@ -38,14 +40,15 @@ def register_startup_event(
     :return: function that actually performs actions.
     """
 
-    @app.on_event("startup")
-    async def _startup() -> None:  # noqa: WPS430
+    @asynccontextmanager
+    async def lifespan():
+
         app.middleware_stack = None
         _setup_db(app)
         app.middleware_stack = app.build_middleware_stack()
-        pass  # noqa: WPS420
+        yield
 
-    return _startup
+    return lifespan
 
 
 def register_shutdown_event(
@@ -58,10 +61,11 @@ def register_shutdown_event(
     :return: function that actually performs actions.
     """
 
-    @app.on_event("shutdown")
-    async def _shutdown() -> None:  # noqa: WPS430
-        await app.state.db_engine.dispose()
+    @asynccontextmanager
+    async def lifespan():
+        yield
 
+        await app.state.db_engine.dispose()
         pass  # noqa: WPS420
 
-    return _shutdown
+    return lifespan
