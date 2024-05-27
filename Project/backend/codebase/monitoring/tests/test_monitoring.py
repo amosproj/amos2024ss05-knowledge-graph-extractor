5152a9e0366b09a2eebad568ba7e96528b3e7527
Nikolas Rauscher <nikolas.rauscher@gmail.com>
2024-05-27 18:45:29 +0200

    ✨make format✨
    Signed-off-by: Nikolas Rauscher <nikolas.rauscher@gmail.com>

--- Project/backend/codebase/monitoring/tests/test_monitoring.py
+++ Project/backend/codebase/monitoring/tests/test_monitoring.py
@@ -1,4 +1,3 @@
-
 import os
 import pytest
 
@@ -12,7 +11,6 @@ def test_health_check(client):
     url = client.app.url_path_for("health_check")
     response = client.get(url)
     assert response.status_code == status.HTTP_200_OK
-
 
 
 @pytest.mark.api
@@ -44,4 +42,3 @@ def test_upload_pdf(client):
     # Remove the test files
     os.remove(file_path)
     os.remove(saved_file_path)
-
