9c719f0996e753d7e567c79cd6140a5a9155ba29
Nikolas Rauscher <nikolas.rauscher@gmail.com>
2024-05-27 15:56:03 +0200

    added ORS-configuration #62
    Signed-off-by: Nikolas Rauscher <nikolas.rauscher@gmail.com>

--- Project/backend/codebase/main.py
+++ Project/backend/codebase/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from lifetime import register_startup_event, register_shutdown_event
 from router import api_router
 from settings.defaults import PROJECT_NAME
@@ -12,6 +12,20 @@ app = FastAPI(
 )
 register_startup_event(app)
 register_shutdown_event(app)
+
+# CORS-configuration
+origins = [
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Main router for the API.
 app.include_router(router=api_router, prefix="/api")
