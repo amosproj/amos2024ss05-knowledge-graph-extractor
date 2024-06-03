d71cef764c05c64981419b42eeae3e2df4b8c2a1
Sandeepkumar Ramesh <166813285+Sandeep-kumar-Ramesh@users.noreply.github.com>
2024-06-03 08:07:19 +0000

    generate API added
    Signed-off-by: Sandeepkumar Ramesh <166813285+Sandeep-kumar-Ramesh@users.noreply.github.com>

--- Project/frontend/src/components/Home/index.tsx
+++ Project/frontend/src/components/Home/index.tsx
@@ -4,20 +4,58 @@ import { useNavigate } from "react-router-dom";
 import logo from "../../assets/team-logo.svg";
 import Upload from "../Upload";
 import "./index.css";
+import { GENERATE_API_PATH } from "../../constant";
 
 
 function Home() {
   const [disableGenerate, setDisableGenerate] = useState(true);
+  const [fileId, setFileId] = useState("");
+
   const navigate = useNavigate();
 
-  const handleAddFile = (error: any, file: File) => {
+  const handleAddFile = (error: any, file: any) => {
     // TODO: This needs to be changed once we have the CORS ready.
-    if (error)
+    console.log(file)
+
+    if (!error) {
+      const fileId = JSON.parse(file.serverId as any).id;
+      setFileId(fileId);
       setDisableGenerate(false)
+    }
   }
 
   const handleGenerateGraph = () => {
-    navigate('/graph');
+    const API = GENERATE_API_PATH.replace(":fileId", fileId);   //TODO: vite
+    fetch(API, {
+
+      // Adding method type 
+      method: "POST",
+
+      // Adding body or contents to send 
+      // body: JSON.stringify({ 
+      //     title: "foo", 
+      //     body: "bar", 
+      //     userId: 1 
+      // }), 
+
+      // Adding headers to the request 
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+
+      // Converting to JSON 
+      .then(response => response.json())
+
+      // Displaying results to console 
+      .then(json => {
+        console.log(json);
+        //TODO: Check status generated
+        navigate(`/graph/${fileId}`);
+      })
+      .catch(e => navigate(`/graph/${fileId}`) //TODO: console.log(e)
+      )
+
   }
 
   return (
@@ -30,7 +68,7 @@ function Home() {
           handleAddFile={handleAddFile}
         />
         <div>
-          <button className="primary_btn" disabled={disableGenerate} onClick={handleGenerateGraph}>Generate Graph</button>
+          <button className="primary_btn" disabled={!fileId} onClick={handleGenerateGraph}>Generate Graph</button>
         </div>
       </div>
     </main>
