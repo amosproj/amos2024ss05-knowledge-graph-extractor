b966d033679d2d1cffd1e9aab3b5b9ba89df8fbc
Nikolas Rauscher <nikolas.rauscher@gmail.com>
2024-06-03 18:16:16 +0200

    func description #79
    Signed-off-by: Nikolas Rauscher <nikolas.rauscher@gmail.com>

--- Project/backend/codebase/graph_creator/graph_creator_main.py
+++ Project/backend/codebase/graph_creator/graph_creator_main.py
@@ -8,45 +8,75 @@ from graph_creator import llm_handler
 from graph_creator import graph_handler
 from graph_creator.services import netx_graphdb
 
+
 def process_file_to_graph(g_job: GraphJob):
-    #extract entities and relations
+    """
+    Processes a file to create a graph.
+
+    Args:
+        g_job (GraphJob): The GraphJob object containing information about the file and graph.
+
+    Returns:
+        None
+    """
+    # extract entities and relations
     entities_and_relations = process_file_to_entities_and_relations(g_job.location)
     uuid = g_job.id
     create_and_store_graph(uuid, entities_and_relations)
 
+
 def process_file_to_entities_and_relations(file):
-    #todo: implement actual processing
-    #chunks = pdf_handler.process_pdf_into_chunks(file)
+    """
+    Process the given file to extract entities and relations.
+
+    Args:
+        file (str): The path to the file to be processed.
+
+    Returns:
+        list: A list of dictionaries representing the extracted entities and relations.
+    """
+    # todo: implement actual processing
+    # chunks = pdf_handler.process_pdf_into_chunks(file)
 
     with open("tests/data/llmExtractedInformation.json") as file:
         entities_and_relations = json.load(file)
-    
-    #flatten the list ba adding attribute chunk_id
+
+    # flatten the list ba adding attribute chunk_id
     flattened_data = []
     for j in range(len(entities_and_relations)):
         id = j
         for i in range(len(entities_and_relations[j])):
-            entities_and_relations[j][i]['chunk_id'] = str(id)
+            entities_and_relations[j][i]["chunk_id"] = str(id)
             flattened_data.append(entities_and_relations[j][i])
-    
+
     return flattened_data
 
+
 def create_and_store_graph(uuid, entities_and_relations):
-    #convert data to dataframe
+    """
+    Create and store a graph based on the given entities and relations.
+
+    Parameters:
+    - uuid (str): The unique identifier for the graph.
+    - entities_and_relations (list): A list of dictionaries representing the entities and relations.
+
+    Returns:
+    None
+    """
+    # convert data to dataframe
     df_e_and_r = pandas.DataFrame(entities_and_relations)
 
-    #combine knowlege graph pieces
+    # combine knowledge graph pieces
     print(df_e_and_r)
     combined = graph_handler.connect_with_chunk_proximity(df_e_and_r)
 
-    #get graph db service
+    # get graph db service
     graph_db_service = netx_graphdb.NetXGraphDB()
 
-    #read entitries and relations
+    # read entities and relations
     graph = graph_db_service.create_graph_from_df(uuid, combined)
 
-    #safe graph as file
+    # save graph as file
     graph_db_service.save_graph(uuid, graph)
 
     print("Done with processing")
-
