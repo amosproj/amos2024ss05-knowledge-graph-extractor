7b8aab97c3a9c94bc5fe22fa80d8bd1a9fb72173
Filipe Borges <filipe.af.borges@gmail.com>
2024-06-11 10:30:35 +0200

    refined transform_llm_output_to_dict function
    Signed-off-by: Filipe Borges <filipe.af.borges@gmail.com>

--- Project/backend/codebase/graph_creator/services/json_handler.py
+++ Project/backend/codebase/graph_creator/services/json_handler.py
@@ -3,14 +3,30 @@ import re
 
 
 def transform_llm_output_to_dict(llm_output) -> dict:
-    # todo- Refactor and extract into separate task
-    #  needs to become more reliable
+    # Remove any leading comments or unnecessary text
+    llm_output = re.sub(r"^.*?(\[)", "[", llm_output, flags=re.DOTALL)
 
+    # Remove any trailing characters after the JSON structure
+    llm_output = re.sub(r"(\]).*$", "]", llm_output, flags=re.DOTALL)
+
+    # Correct double commas
+    llm_output = llm_output.replace(",,", ",")
+
+    # Remove any comments inside JSON
+    llm_output = re.sub(r"//.*?\n|/\*.*?\*/", "", llm_output, flags=re.DOTALL)
+
+    # Remove extra commas before closing brackets/parentheses
+    llm_output = re.sub(r",\s*(\}|\])", r"\1", llm_output)
+
+    # Find the JSON array in the string
     x = re.search(r"\[.*?\]", llm_output, re.DOTALL)
     if x is None:
         return {}
+
+    # Try to load the JSON
     try:
         relation = json.loads(x.group(0))
     except json.JSONDecodeError:
         return {}
+
     return relation
