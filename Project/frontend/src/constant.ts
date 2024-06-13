35eca787edda4e5bab808b5f90111e07c67df6da
Yash Bhesaniya <yashbhesaniya1999@gmail.com>
2024-06-13 14:24:17 +0200

    refactor: update GRAPH_DELETE_API_PATH constant
    The GRAPH_DELETE_API_PATH constant in constant.ts was updated to remove the fileId parameter. This change aligns with the recent deletion of the upload document feature.
    
    Signed-off-by: Yash Bhesaniya <yashbhesaniya1999@gmail.com>

--- Project/frontend/src/constant.ts
+++ Project/frontend/src/constant.ts
@@ -2,7 +2,7 @@ export const UPLOAD_API_PATH = '/api/graph/upload';
 export const GENERATE_API_PATH = '/api/graph/create_graph/:fileId';
 export const VISUALIZE_API_PATH = '/api/graph/visualize/:fileId';
 export const GRAPH_LIST_API_PATH = '/api/graph/graph_jobs';
-export const GRAPH_DELETE_API_PATH = '/api/graph/graph_jobs/:fileId';
+export const GRAPH_DELETE_API_PATH = '/api/graph/graph_jobs';
 
 export enum GraphStatus {
   DOC_UPLOADED = 'document_uploaded',
