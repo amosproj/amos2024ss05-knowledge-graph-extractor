afb4cbb49d9d4162450259df328538c7a61afe9c
Sandeep-kumar-Ramesh <sandeepkumar.ramesh@fau.de>
2024-06-20 16:14:30 +0530

    delete handled for upload
    Signed-off-by: Sandeep-kumar-Ramesh <sandeepkumar.ramesh@fau.de>

--- Project/frontend/src/components/Upload/index.tsx
+++ Project/frontend/src/components/Upload/index.tsx
@@ -3,12 +3,12 @@ import 'filepond/dist/filepond.min.css';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 
 import './index.css';
-import { UPLOAD_API_PATH } from '../../constant';
+import { GRAPH_DELETE_API_PATH, UPLOAD_API_PATH } from '../../constant';
 
 registerPlugin(FilePondPluginFileValidateType);
 
-interface FilePondFile {
-  serverId: string;
+export interface FilePondFile {
+  id: string;
 }
 
 interface FilePondError {
@@ -19,22 +19,56 @@ interface FilePondError {
 type UploadProps = {
   handleAddFile: (error: FilePondError | null, file: FilePondFile) => void;
   handleRemoveFile: () => void;
+  handleDeleteFile: () => void;
   pondRef: React.RefObject<any>;
 };
 
 function Upload(props: UploadProps) {
+  
+  const server= {
+    url: `${import.meta.env.VITE_BACKEND_HOST}`,
+    process: {
+      url: `${UPLOAD_API_PATH}`,
+      method: 'POST',
+      onload: (response: FilePondFile) => {
+        return response.id; // The server ID of the uploaded file
+      },
+      onerror: (response: FilePondError) => {
+        console.error('Error uploading file:', response);
+      }
+    },
+    revert: (fileId: string, load: any, error: any) => {
+      const API = `${import.meta.env.VITE_BACKEND_HOST}${GRAPH_DELETE_API_PATH.replace(':fileId', fileId)}`;
+      fetch(API, {
+        method: 'DELETE'
+      })
+      .then(response => {
+        if (response.ok) {
+          load();
+          props.handleDeleteFile();
+        } else {
+          error('Error deleting file');
+        }
+      })
+      .catch((err:FilePondError) => {
+        error('Error deleting file');
+      });
+    }
+  };
+
   return (
     <section className="upload_wrapper">
       <FilePond
         ref={props.pondRef}
         allowMultiple={false}
-        server={`${import.meta.env.VITE_BACKEND_HOST}${UPLOAD_API_PATH}`}
+        server={server}
         name="file"
         labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
         acceptedFileTypes={['application/pdf']}
         credits={false}
-        onprocessfile={(error, file) =>
-          props.handleAddFile(error as FilePondError, file as FilePondFile)
+        onprocessfile={(error: any, file: any) =>
+    //      props.handleAddFile(error as FilePondError, file as FilePondFile)
+        console.log()
         }
         onremovefile={props.handleRemoveFile}
       />
