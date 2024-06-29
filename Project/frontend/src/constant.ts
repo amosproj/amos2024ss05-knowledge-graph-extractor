29e131b4baba3d08049e4000a132af23c451f11d
Yash Bhesaniya <yashbhesaniya1999@gmail.com>
2024-06-29 13:18:32 +0200

    feat: add API path for graph keywords
    Added a new constant `KEYWORDS_API_PATH` to the `constant.ts` file to define the API path for retrieving graph keywords. This will be used in the graph analysis feature.
    
    Ref: #123
    Signed-off-by: Yash Bhesaniya <yashbhesaniya1999@gmail.com>

--- Project/frontend/src/constant.ts
+++ Project/frontend/src/constant.ts
@@ -3,6 +3,7 @@ export const GENERATE_API_PATH = '/api/graph/create_graph/:fileId';
 export const VISUALIZE_API_PATH = '/api/graph/visualize/:fileId';
 export const GRAPH_LIST_API_PATH = '/api/graph/graph_jobs';
 export const GRAPH_DELETE_API_PATH = '/api/graph/graph_jobs/:fileId';
+export const KEYWORDS_API_PATH = '/api/graph/graph_keywords/:fileId'; 
 
 export enum GraphStatus {
   DOC_UPLOADED = 'document_uploaded',
