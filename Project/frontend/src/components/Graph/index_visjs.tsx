eb41ec6af94095aa73a3d4595d998c65d04482e2
Yash Bhesaniya <yashbhesaniya1999@gmail.com>
2024-07-02 14:14:50 +0200

    Revert "Feat/hover and code refining"

--- Project/frontend/src/components/Graph/index_visjs.tsx
+++ Project/frontend/src/components/Graph/index_visjs.tsx
@@ -49,7 +49,6 @@ const VisGraph: React.FC<{ graphData: GraphData; options: Options }> = ({
         label: node.label || node.id,
         shape: 'dot',
         size: 25,
-        title: `Found in pages: ${node.pages}`,
         color: {
           background: '#69b3a2',
           border: '#508e7f',
@@ -170,7 +169,6 @@ const GraphVisualization: React.FC = () => {
     },
     interaction: {
       hover: true,
-      tooltipDelay: 30,
       zoomView: true,
       dragView: true,
       selectConnectedEdges: false,
@@ -312,24 +310,16 @@ const GraphVisualization: React.FC = () => {
         maxWidth={450}
         minWidth={450}
       >
-        <Stack sx={{ fontSize: '25px', margin: '15px' }}>
+        <Typography variant="h5" gutterBottom>
           Graph Information
-        </Stack>
-        <Stack sx={{ fontSize: '16px', margin: '10px' }}>
-          Document Name:
-          <Typography sx={{ marginTop: '2px' }}>
-            {graphData.document_name}
-          </Typography>
-        </Stack>
-        <Stack sx={{ fontSize: '16px', margin: '10px' }}>
-          Created at:
-          <Typography sx={{ marginTop: '2px' }}>
-            {formattedDate} {formattedTime}
-          </Typography>
-        </Stack>
-        <Stack sx={{ fontSize: '16px', margin: '10px' }}>
-          Graph keywords:
-          <Box sx={{ marginTop: '4px' }}>
+        </Typography>
+        <Typography variant="body1">
+          Document Name: <br /> {graphData.document_name}
+          <br /> <br />
+          Created at: <br /> {formattedDate} {formattedTime}
+          <br /> <br />
+          Graph keywords: <br />
+          <Box sx={{ marginBottom: 1 }}>
             {keywords.map((keyword) => (
               <Chip
                 key={keyword}
@@ -340,7 +330,7 @@ const GraphVisualization: React.FC = () => {
               />
             ))}
           </Box>
-        </Stack>
+        </Typography>
         <TextField
           className="search_text_field"
           placeholder="Search for keywords"
@@ -373,7 +363,7 @@ const GraphVisualization: React.FC = () => {
             Graph Visualization
           </Typography>
           <Select
-            size="small"
+            size='small'
             value={layout}
             onChange={(e) => setLayout(e.target.value as string)}
             style={{ marginBottom: '20px' }}
