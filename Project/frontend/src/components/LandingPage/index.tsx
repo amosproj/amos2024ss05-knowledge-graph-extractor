d3e0003e656ebcea1ba98acebb5a2f0e16d98406
Filipe Borges <filipe.af.borges@gmail.com>
2024-07-12 16:45:30 +0200

    made with <3
    Signed-off-by: Filipe Borges <filipe.af.borges@gmail.com>

--- Project/frontend/src/components/LandingPage/index.tsx
+++ Project/frontend/src/components/LandingPage/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
-import { Button, Container, Stack, Typography } from '@mui/material';
-
+import { Button, Container, Stack, Typography, Link } from '@mui/material';
 import GraphList from '../GraphList';
 import { useNavigate } from 'react-router-dom';
 
@@ -10,38 +8,28 @@ const LandingPage = () => {
   return (
     <Container maxWidth="lg">
       <Stack direction={'column'} spacing={1} sx={{ padding: 1 }}>
-        <Stack
-          direction={'column'}
-          alignItems={'center'}
-          alignSelf={'stretch'}
-          py={2}
-        >
+        <Stack direction={'column'} alignItems={'center'} alignSelf={'stretch'} py={2}>
           <Typography variant="h4" component="h4" gutterBottom>
             Welcome to Graph Masters
           </Typography>
-          <Typography
-            variant="h6"
-            component="h6"
-            gutterBottom
-            color={(theme) => theme.palette.text.secondary}
-          >
+          <Typography variant="h6" component="h6" gutterBottom color={(theme) => theme.palette.text.secondary}>
             Create, View & Query Knowledge Graphs
           </Typography>
         </Stack>
         <Stack direction="row" justifyContent="flex-end">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => navigate('/upload')}
-          >
+          <Button variant="contained" color="primary" onClick={() => navigate('/upload')}>
             Create Graph
           </Button>
         </Stack>
       </Stack>
-
       <section className="graph_list_container">
         <GraphList />
       </section>
+      {/* Footer message */}
+      <Typography variant="body2" color="textSecondary" align="center" sx={{ mt: 30 }}>
+        Made with 💙 at FAU and TU Berlin.
+      </Typography>
+      
     </Container>
   );
 };
